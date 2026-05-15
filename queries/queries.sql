a5d2309234dd39647f2b6746995c1460d669663b
alangiralt-dot <alangiralt@telefonica.net>
2026-05-15 03:09:48 +0200

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -5,7 +5,7 @@ SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-SELECT * FROM producto;
+SELECT codigo, nombre, precio, codigo_fabricante FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
 SELECT nombre, precio AS precio_eur, precio * 1 AS precio_usd FROM producto;
@@ -14,19 +14,19 @@ SELECT nombre, precio AS precio_eur, precio * 1 AS precio_usd FROM producto;
 SELECT nombre AS 'nom del producte', precio AS euros, ROUND(precio * 1.1, 2) AS dòlars FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
-SELECT UPPER(nombre), precio FROM producto;
+SELECT UPPER(nombre) AS nombre, precio FROM producto;
 
 -- 7. Llista els noms (nombre) i els preus (precio) dels productes de la taula producto, convertint els noms a minúscula.
-SELECT LOWER(nombre), precio FROM producto;
+SELECT LOWER(nombre) AS nombre, precio FROM producto;
 
 -- 8. Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant (iniciales).
 SELECT nombre, UPPER(SUBSTRING(nombre, 1, 2)) AS iniciales FROM fabricante;
 
 -- 9. Llista els noms i els preus dels productes, arrodonint el valor del preu (precio)
-SELECT nombre, ROUND(precio) FROM producto;
+SELECT nombre, ROUND(precio) AS precio FROM producto;
 
 -- 10. Llista els noms i els preus de tots els productes (precio truncado) de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
-SELECT nombre, TRUNCATE(precio, 0) AS precio_truncado FROM producto;
+SELECT nombre, TRUNCATE(precio, 0) AS `precio truncado` FROM producto;
 
 -- 11. Mostra una llista amb els codis dels fabricants que apareixen a la taula producto, incloent possibles repeticions.
 SELECT codigo_fabricante FROM producto;
